da08d351f3929d4db8a9ea216c8ce53c3e1550c2
GrungeElFz <elf0114_@hotmail.com>
2022-10-04 14:16:54 -0500

    Modified: api/tags.js

---
+++ api/tags.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { client, getPostsByTagName } = require('../db');
+const tagsRouter = express.Router();
+
+tagsRouter.use((req, res, next) => {
+    console.log("A request is being made to /tags");
+
+    next();
+});
+
+tagsRouter.get('/', async (req, res, next) => {
+    try {
+        const tags = await client.query(
+            SELECT * FROM tags
+        )
+
+        res.send({
+            tags
+        });
+
+    } catch (error) {
+        next()
+    }
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    try {
+        const tags = await getPostsByTagName()
+
+        return (
+            tags,
+            res.send({ posts: tags })
+        )
+
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+});
+
+module.exports = tagsRouter;
